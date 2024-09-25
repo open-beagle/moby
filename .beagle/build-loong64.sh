ff3dada4dd2d72b9afe8a1421246004d668090e9
shucheng <shucheng@bd-apaas.com>
2024-09-25 18:14:20 +0800

    v27.3.1 loong64 support

--- .beagle/build-loong64.sh
+++ .beagle/build-loong64.sh
@@ -10,9 +10,9 @@ export DOCKER_EXPERIMENTAL=1
 export DOCKER_LDFLAGS='-s -w'
 export DOCKER_GITCOMMIT=$(git rev-parse --short HEAD)
 
-git apply .beagle/v25.0.5-add-seccomp-support-for-loong64.patch
+git apply .beagle/v27.3.1-loong64.patch
 export GOARCH=loong64
 hack/make.sh binary
 mkdir -p .tmp/$GOARCH
 mv bundles/binary-daemon/* .tmp/$GOARCH/
-git apply -R .beagle/v25.0.5-add-seccomp-support-for-loong64.patch
+git apply -R .beagle/v27.3.1-loong64.patch
